983f62a33bbf8e98f53bb36be42364975fad7d27
Arthur Gartner <Arthur.Gartner@gmail.com>
2023-09-11 19:45:31 -0600

    Additional updates

--- typescriptYoutube/03PURETS/dist/index.js
+++ typescriptYoutube/03PURETS/dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("typescript is here");
+// class User {
+//   email: string;
+//   name: string;
+//   //   city: string = "";
+//   readonly city = "Denver";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// const arthur = new User("example@example.com", "Arthur");
+// arthur.city = "Denver";
+class User {
+    constructor(email, name // private userId: string
+    ) {
+        this.email = email;
+        this.name = name;
+        this.email = email;
+        this.name = name;
+    }
+    //Getter example
+    get getAppleEmail() {
+        return `Apple${this.email}`;
+    }
+}
--- typescriptYoutube/03PURETS/src/index.ts
+++ typescriptYoutube/03PURETS/src/index.ts
@@ -1,1 +1,29 @@
-console.log("typescript is here");
+// class User {
+//   email: string;
+//   name: string;
+//   //   city: string = "";
+//   readonly city = "Denver";
+
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// const arthur = new User("example@example.com", "Arthur");
+// arthur.city = "Denver";
+
+class User {
+  constructor(
+    public email: string,
+    public name: string // private userId: string
+  ) {
+    this.email = email;
+    this.name = name;
+  }
+
+  //Getter example
+  get getAppleEmail(): string {
+    return `Apple${this.email}`;
+  }
+}
